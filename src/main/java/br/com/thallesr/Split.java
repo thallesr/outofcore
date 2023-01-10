0a77d8be1efc6013e87a6517406d59b9fe762148
Thalles Rodrigues <trodrigues@alpha-sense.com>
2023-01-10 10:58:45 +0200

    KB and B mistake corrected

--- src/main/java/br/com/thallesr/Split.java
+++ src/main/java/br/com/thallesr/Split.java
@@ -8,26 +8,31 @@ import java.util.Random;
 
 public class Split {
     public static void main(String[] args) throws IOException {
-        String filename = args[0];
-        String fileSizeString = args[1];
-        Integer fileSize = Integer.valueOf(fileSizeString);
-        int[] memorySpace = new int[fileSize / 4];
+        try {
+            String filename = args[0];
+            String fileSizeStringKB = args[1];
+            Integer fileSizeKB = Integer.valueOf(fileSizeStringKB);
+            int[] memorySpace = new int[fileSizeKB * 256]; //    fileSizeKB * 1024 /4
 
-        RandomAccessFile file = new RandomAccessFile(filename, "rw");
-        for (int i = 0; i < fileSize / 4; i++) {
-            memorySpace[i] = file.readInt();
+            RandomAccessFile file = new RandomAccessFile(filename, "r");
+            for (int i = 0; i < fileSizeKB / 4; i++) {
+                memorySpace[i] = file.readInt();
+            }
+            file.close();
+            Arrays.sort(memorySpace);
+
+            String fileOutput = "out1.data";
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileOutput));
+            for (int value : memorySpace) {
+                bos.write(ByteBuffer.allocate(4).putInt(value).array());
+            }
+            bos.flush();
+            bos.close();
         }
-        file.close();
-        Arrays.sort(memorySpace);
+        catch (Exception e ){
+            e.printStackTrace();
 
-        String fileOutput = "out1.data";
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileOutput));
-        for (int value : memorySpace) {
-            bos.write(ByteBuffer.allocate(4).putInt(value).array());
         }
-        bos.flush();
-        bos.close();
-
 
     }
 }
